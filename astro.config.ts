184a751146fabd232983aa59a1fa79e111fcad4c
Igor Eremin <eremin.igor@icloud.com>
2024-10-06 21:13:30 -0700

    i am testing everything to fix

--- astro.config.ts
+++ astro.config.ts
@@ -97,6 +97,7 @@ export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
 	site: "https://igor-eremn.github.io",
 	vite: {
+		base: "/portfolio/",
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],
 		},
