0aa01106c879f91a9a1675f9e801f74f097a4c2e
Igor Eremin <eremin.igor@icloud.com>
2024-10-06 21:10:17 -0700

    it is building and deploying but i get 404

--- astro.config.ts
+++ astro.config.ts
@@ -95,7 +95,7 @@ export default defineConfig({
 	// https://docs.astro.build/en/guides/prefetch/
 	prefetch: true,
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://igor-eremn.github.io/portfolio",
+	site: "https://igor-eremn.github.io",
 	vite: {
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],
